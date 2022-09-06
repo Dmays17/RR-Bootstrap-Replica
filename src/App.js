031ebb73915921367853c97245004752fbc8ddf9
Dmays17 <101609429+Dmays17@users.noreply.github.com>
2022-09-06 17:06:09 -0500

    3 files add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import Background from './Background';
+import TopBar from './TopBar';
+import StoreListing from './StoreListing';
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <TopBar></TopBar>
+      <Background></Background>
+      <StoreListing></StoreListing>
     </div>
   );
 }
---
+++ src/Background.js
@@ -0,0 +1,9 @@
+function Background(){
+    return(
+    <div>
+        <img src="https://www.instacart.com/assets/homepage-hero-background-ec75de0ea80198801094497b46ad920b.jpg"
+        alt="Background pic"></img>
+    </div>
+    )
+}
+export default Background
